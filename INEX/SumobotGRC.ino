d73a6bc74e7ac5763804106b07bae4953fb7897a
Al Razelle A. Ramores <al.razelle.a.ramores@gmail.com>
2024-10-06 12:11:12 +0800

    Updated loop function

--- INEX/SumobotGRC.ino
+++ INEX/SumobotGRC.ino
@@ -1,15 +1,43 @@
 #include<POP32.h> //library
+
 void setup() {
-  // put your setup code here, to run once: global run once
-SW_OK_press();// on
-oled.text(5,5, "Al");
-oled.show();
+    // put your setup code here, to run once: global run once
+    SW_OK_press();// on
+    oled.text(5, 5, "Al");
+    oled.show();
 }
 
-void loop() {// local area
-  // put your main code here, to run repeatedly:
-//movement forward, backward, left, right, spin L, spin R, 
-motor(1,50); //FD(100) ay 4 motors small ay 2 motors
-delay (500); //1000=1 sec  duration of movement
-motor(34,50); delay(100);
+void loop() {
+    glcd(2, 0
+    "FORWARD 50%  ");
+    FD(50);
+    delay(1000);
+    AO();
+    sw_OK_press();
+    glcd(2, 0, "BACKWARD 50% ");
+    BK(50);
+    delay(1000);
+    AO();
+    sw_OK_press();
+    glcd(2, 0, "SPIN LEFT 50% ");
+    SL(50);
+    delay(1000);
+    AO();
+    sw_OK_press();
+    glcd(2, 0, "SPIN RIGHT 50%");
+    SR(50);
+    delay(1000);
+    AO();
+    sw_OK_press();
+    glcd(2, 0, "TURN LEFT 50% ");
+    TL(50);
+    delay(1000);
+    A0();
+    sw_OK_press();
+    glcd(2, 0, "TURN RIGHT 50%");
+    TR(50);
+    delay(1000);
+    AO();
+    sw_OK_press();
+
 }
