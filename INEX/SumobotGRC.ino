9e4fa86cb579cf64140a8e2c3e65f039a469e981
Rafael Jay Ramores <rafael.p.ramores@gmail.com>
2024-10-16 14:20:29 +0800

    fixed toggle direction

--- INEX/SumobotGRC.ino
+++ INEX/SumobotGRC.ino
@@ -1,26 +1,40 @@
 #include "POP32.h"
 
 
+/* ============= Configurations ================= */
 int EDGE_LEFT_THRESHOLD = 900;
 int EDGE_RIGHT_THRESHOLD = 900;
-int FRONT_THRESHOLD = 250;
+int FRONT_THRESHOLD = 900;
 
 int ATTACK_SPEED = 100;
 
 int BACKOFF_SPEED = 80;
-int BACKOFF_SPIN_SPEED = 60;
 int BACKOFF_DELAY = 500;
+int BACKOFF_SPIN_SPEED = 60;
 int BACKOFF_SPIN_DELAY = 1000;
 
-int SEARCH_SPIN_SPEED = 50;
+int SEARCH_SPEED = 60;
+int SEARCH_TURN_SPEED = 50;
+int SEARCH_DELAY = 1000;
+int TOGGLE_DIRECTION_COUNT = 3; // change search direction after n count
+/* ============= Configurations ================= */
 
+
+/* ============= Constants ================= */
 int LEFT = 0;
 int RIGHT = 1;
+String ACTION_SEARCH = "SEARCH ";
+String ACTION_ATTACK = "ATTACK ";
+String ACTION_BACKOFF = "BACKOFF";
+/* ============= Constants ================= */
 
 
+/* ============= Default Values ================= */
 int direction = LEFT;
-string action = ""; // SEARCH/ATTACK/BACKOFF
+int switchDirectionCount = 0;
+String action = ""; // SEARCH/ATTACK/BACKOFF
 unsigned int data = 0; // stores the controller response
+/* ============= Default Values ================= */
 
 
 void setup() {
@@ -36,11 +50,11 @@ void loop() {
     if (analog(0) > EDGE_LEFT_THRESHOLD) {
         // edge is detected on the left, backoff and u-turn to the right
         backoff(RIGHT);
-        direction ^= 1;
+        direction = RIGHT;
     } else if (analog(1) > EDGE_RIGHT_THRESHOLD) {
         // edge is detected on the right, backoff and u-turn to the left
         backoff(LEFT);
-        direction ^= 1;
+        direction = LEFT;
     } else if(analog(2) > FRONT_THRESHOLD) {
         attack();
     } else {
@@ -49,21 +63,30 @@ void loop() {
 }
 
 void attack() {
-    action = "ATTACK";
+    action = ACTION_ATTACK;
     FD(ATTACK_SPEED);
 }
 
-void search(int direction) {
-    action = "SEARCH";
-    if (direction == LEFT) {
-        SL(SEARCH_SPIN_SPEED);
+void search(int searchDirection) {
+    action = ACTION_SEARCH;
+    if (searchDirection == LEFT) {
+        TL(SEARCH_TURN_SPEED);
     } else {
-        SR(SEARCH_SPIN_SPEED);
+        TR(SEARCH_TURN_SPEED);
     }
+    delay(SEARCH_DELAY);
+
+    switchDirectionCount++;
+    if (switchDirectionCount == TOGGLE_DIRECTION_COUNT) {
+      direction ^= 1; // toggle search direction
+      switchDirectionCount = 0;
+    }
+
+    FD(SEARCH_SPEED);
 }
 
 void backoff(int direction) {
-    action = "BACKOFF";
+    action = ACTION_BACKOFF;
     BK(BACKOFF_SPEED);
     delay(BACKOFF_DELAY);
 
@@ -82,8 +105,8 @@ void printSensorReadings() {
     oled.text(0, 0, "%d ", analog(0)); // left line sensor
     oled.text(1, 0, "%d ", analog(1)); // right line sensor
     oled.text(2, 0, "%d ", analog(2)); // front object sensor
-    oled.text(3, 0, "%d ", action);
-    oled.text(4, 0, "%d ", direction);
+    oled.text(3, 0, action.c_str());
+    oled.text(4, 0, direction == 0 ? "LEFT " : "RIGHT");
     oled.display();
 }
 
