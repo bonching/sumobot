83bfb6ed831d7360058b817c1d24cb2d866b5f9d
Rafael Jay Ramores <rafael.p.ramores@gmail.com>
2024-10-26 17:46:44 +0800

    testing config tweak

--- INEX/SumobotGRC.ino
+++ INEX/SumobotGRC.ino
@@ -20,7 +20,7 @@ int TURNAROUND_SPIN_SPEED = 100;
 int TURNAROUND_SPIN_DELAY = 100;
 
 int SEARCH_SPEED = 100;
-int SEARCH_SPIN_SPEED = 60;
+int SEARCH_SPIN_SPEED = 50;
 int SEARCH_SPIN_DELAY = 10;
 int SEARCH_FORWARD_MAX_COUNT = 15; // search forward without turning left/right
 int TOGGLE_DIRECTION_COUNT = 3; // change search direction after n count
