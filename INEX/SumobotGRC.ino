5ea7b1c625d838ab882295f8ddedbe260c002aef
Rafael Jay Ramores <rafael.p.ramores@gmail.com>
2024-10-26 17:12:14 +0800

    testing config tweak

--- INEX/SumobotGRC.ino
+++ INEX/SumobotGRC.ino
@@ -69,11 +69,11 @@ void loop() {
     }  else if(analog(6) > LEFT_THRESHOLD){
         action = ACTION_SEARCH_LEFT;
         SL(SEARCH_SPIN_SPEED);
-        delay(SEARCH_SPIN_DELAY);
+//        delay(SEARCH_SPIN_DELAY);
     } else if(analog(4) > RIGHT_THRESHOLD){
         action = ACTION_SEARCH_RIGHT;
         SR(SEARCH_SPIN_SPEED);
-        delay(SEARCH_SPIN_DELAY);
+//        delay(SEARCH_SPIN_DELAY);
 //    }  else if(analog(3) > BACK_THRESHOLD){
 //        SR(TURNAROUND_SPIN_SPEED);
 //        delay(TURNAROUND_SPIN_DELAY);
